94ae2ffcab8a250579e7acda3d39bdaa83361892
“Bilal-Ait-Si-Ali” <bilalaitsiali2@gmail.com>
2024-03-10 04:17:33 +0000

    Chapter 6: Finally, Some UI To Play With

--- tutorials/estate/models/estate_property.py
+++ tutorials/estate/models/estate_property.py
@@ -5,17 +5,35 @@ class EstateProperty(models.Model):
     _name = "estate.property"
     _description = "Estate Property"
 
-    name = fields.Char(required=True)
+    name = fields.Char("Title", required=True)
     description = fields.Text()
     postcode = fields.Char()
-    date_availability = fields.Date()
+    date_availability = fields.Date("Available From", copy=False,
+                                    default=lambda self: fields.Datetime.add(
+                                        value=fields.Datetime.today(),
+                                        months=3
+                                    ))
     expected_price = fields.Float(required=True)
-    selling_price = fields.Float()
-    bedrooms = fields.Integer()
-    living_area = fields.Integer()
+    selling_price = fields.Float(readonly=True, copy=False)
+    bedrooms = fields.Integer(default=2)
+    living_area = fields.Integer("Living Area (sqm)")
     facades = fields.Integer()
     garage = fields.Boolean()
     garden = fields.Boolean()
-    garden_area = fields.Integer()
+    garden_area = fields.Integer("Garden Area (sqm)")
     garden_orientation = fields.Selection(
         [('north', 'North'), ('south', 'South'), ('east', 'East'), ('west', 'West')])
+    state = fields.Selection(
+        required=True,
+        copy=False,
+        string='State',
+        selection=[
+            ('new', 'New'),
+            ('offer received', 'Offer Received'),
+            ('offer accepted', 'Offer Accepted'),
+            ('sold', 'Sold'),
+            ('canceled', 'Canceled'),
+        ],
+        default='new'
+    )
+    active = fields.Boolean(default=True)
