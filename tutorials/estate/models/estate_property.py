956207fb768c5a6a98fb0e644e1f1324fb055d62
“Bilal-Ait-Si-Ali” <bilalaitsiali2@gmail.com>
2024-03-11 01:13:38 +0000

    Chapter 8: Relations Between Models

--- tutorials/estate/models/estate_property.py
+++ tutorials/estate/models/estate_property.py
@@ -6,6 +6,8 @@ class EstateProperty(models.Model):
     _description = "Estate Property"
 
     name = fields.Char("Title", required=True)
+    tag_ids = fields.Many2many('estate.property.tag')
+    property_type_id = fields.Many2one("estate.property.type", string="Property Type")
     description = fields.Text()
     postcode = fields.Char()
     date_availability = fields.Date("Available From", copy=False,
@@ -37,3 +39,22 @@ class EstateProperty(models.Model):
         default='new'
     )
     active = fields.Boolean(default=True)
+
+    salesperson_id = fields.Many2one('res.users', string='Salesman', default=lambda self: self.env.user)
+    buyer_id = fields.Many2one('res.partner', copy=False)
+    offer_ids = fields.One2many('estate.property.offer', 'property_id')
+
+
+class EstatePropertyOffer(models.Model):
+    _name = "estate.property.offer"
+    _description = "Estate Property Offer"
+
+    price = fields.Float()
+    status = fields.Selection(copy=False,
+                              selection=[
+                                  ('accepted', 'Accepted'),
+                                  ('refused', 'Refused'),
+                              ]
+                              )
+    partner_id = fields.Many2one('res.partner', raquired=True)
+    property_id = fields.Many2one('estate.property', raquired=True)
