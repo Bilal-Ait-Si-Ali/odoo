48530572c63e1ea83507d4816abef46ba906fb3b
“Bilal-Ait-Si-Ali” <bilalaitsiali2@gmail.com>
2024-03-13 15:41:51 +0000

    Chapter 12: Add The Sprinkles

--- tutorials/estate/models/estate_property.py
+++ tutorials/estate/models/estate_property.py
@@ -1,6 +1,4 @@
 from odoo import fields, models, api, exceptions
-from datetime import date
-from dateutil import relativedelta
 from odoo.exceptions import ValidationError, UserError
 from odoo.tools import float_compare, float_is_zero, float_repr, float_round, float_split, float_split_str
 
@@ -8,6 +6,7 @@ from odoo.tools import float_compare, float_is_zero, float_repr, float_round, fl
 class EstateProperty(models.Model):
     _name = "estate.property"
     _description = "Estate Property"
+    _order = "id desc"
 
     name = fields.Char("Title", required=True)
     tag_ids = fields.Many2many('estate.property.tag')
@@ -29,19 +28,13 @@ class EstateProperty(models.Model):
     garden_area = fields.Integer("Garden Area (sqm)")
     garden_orientation = fields.Selection(
         [('north', 'North'), ('south', 'South'), ('east', 'East'), ('west', 'West')])
-    state = fields.Selection(
-        required=True,
-        copy=False,
-        string='Status',
-        selection=[
-            ('new', 'New'),
-            ('offer received', 'Offer Received'),
-            ('offer accepted', 'Offer Accepted'),
-            ('sold', 'Sold'),
-            ('canceled', 'Canceled'),
-        ],
-        default='new'
-    )
+    state = fields.Selection(required=True, copy=False, string='Status', default='new',
+                             selection=[
+                                 ('new', 'New'),
+                                 ('offer received', 'Offer Received'),
+                                 ('offer accepted', 'Offer Accepted'),
+                                 ('sold', 'Sold'),
+                                 ('canceled', 'Canceled')])
     active = fields.Boolean(default=True)
     salesperson_id = fields.Many2one('res.users', string='Salesman', default=lambda self: self.env.user)
     buyer_id = fields.Many2one('res.partner', copy=False)
@@ -76,6 +69,10 @@ class EstateProperty(models.Model):
         if self.state == 'sold':
             raise exceptions.UserError("Sold property cannot be canceled.")
         self.state = 'canceled'
+        self.selling_price = 0
+        self.buyer_id = False
+        for rec in self.offer_ids:
+            rec.action_refused()
         return
 
     @api.constrains('expected_price', 'selling_price')
@@ -86,51 +83,3 @@ class EstateProperty(models.Model):
             if float_compare(rec.selling_price, rec.expected_price * 0.9, 2) < 0:
                 raise ValidationError(
                     "The selling price must be at least 90% of the expected price! You must reduce the expected price if you want to accept this offer.")
-
-
-class EstatePropertyOffer(models.Model):
-    _name = "estate.property.offer"
-    _description = "Estate Property Offer"
-
-    price = fields.Float()
-    status = fields.Selection(copy=False, selection=[('accepted', 'Accepted'), ('refused', 'Refused')])
-    partner_id = fields.Many2one('res.partner', required=True)
-    property_id = fields.Many2one('estate.property', required=True)
-    validity = fields.Integer('validity (Days)', default=7)
-    date_deadline = fields.Date('Deadline', compute='_compute_date_deadline', inverse='_inverse_compute_date_deadline')
-
-    _sql_constraints = [
-        ('check_price', 'check(price > 0)', 'The price must be strictly positive')
-    ]
-
-    @api.depends('validity')
-    def _compute_date_deadline(self):
-        for rec in self:
-            if rec.validity:
-                rec.date_deadline = (rec.create_date or date.today()) + relativedelta.relativedelta(
-                    days=rec.validity)
-
-    @api.onchange('date_deadline')
-    def _inverse_compute_date_deadline(self):
-        if self.date_deadline:
-            create_date = date.today()
-            if self.create_date:
-                create_date = self.create_date.date()
-            self.validity = (self.date_deadline - create_date).days
-
-    def action_accepted(self):
-        for rec in self:
-            for r in rec.property_id.offer_ids:
-                r.action_refused()
-            rec.status = 'accepted'
-            rec.property_id.selling_price = rec.price
-            rec.property_id.buyer_id = rec.partner_id
-        return
-
-    def action_refused(self):
-        for rec in self:
-            if rec.status == 'accepted':
-                rec.status = 'refused'
-                rec.property_id.selling_price = 0
-                rec.property_id.buyer_id = False
-        return
---
+++ tutorials/estate/models/estate_property_offer.py
@@ -0,0 +1,51 @@
+from odoo import fields, models, api, exceptions
+from datetime import date
+from dateutil import relativedelta
+
+
+class EstatePropertyOffer(models.Model):
+    _name = "estate.property.offer"
+    _description = "Estate Property Offer"
+    _order = "id desc"
+
+    price = fields.Float()
+    status = fields.Selection(copy=False, selection=[('accepted', 'Accepted'), ('refused', 'Refused')])
+    partner_id = fields.Many2one('res.partner', required=True)
+    property_id = fields.Many2one('estate.property', required=True)
+    validity = fields.Integer('validity (Days)', default=7)
+    date_deadline = fields.Date('Deadline', compute='_compute_date_deadline', inverse='_inverse_compute_date_deadline')
+    property_type_id = fields.Many2one(related="property_id.property_type_id", store="True")
+
+    _sql_constraints = [
+        ('check_price', 'check(price > 0)', 'The price must be strictly positive')
+    ]
+
+    @api.depends('validity')
+    def _compute_date_deadline(self):
+        for rec in self:
+            if rec.validity:
+                rec.date_deadline = (rec.create_date or date.today()) + relativedelta.relativedelta(
+                    days=rec.validity)
+
+    @api.onchange('date_deadline')
+    def _inverse_compute_date_deadline(self):
+        if self.date_deadline:
+            create_date = date.today()
+            if self.create_date:
+                create_date = self.create_date.date()
+            self.validity = (self.date_deadline - create_date).days
+
+    def action_accepted(self):
+        for rec in self:
+            for r in rec.property_id.offer_ids:
+                r.action_refused()
+            rec.status = 'accepted'
+            rec.property_id.selling_price = rec.price
+            rec.property_id.buyer_id = rec.partner_id
+            rec.property_id.state = 'offer accepted'
+        return
+
+    def action_refused(self):
+        for rec in self:
+            rec.status = 'refused'
+        return
